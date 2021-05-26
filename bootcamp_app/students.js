a64615cfa0b873cb14760884242302724ccc180f
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-26 05:13:44 +0000

    Add sql injection protection in queries inside of teachers.js and students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 const args = process.argv.slice(2);
 arg1 = args[0]
 arg2 = args[1]
@@ -11,15 +11,24 @@ const pool = new Pool({
 });
 
 
-pool.query(`
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 INNER JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${arg1}%'
-LIMIT ${arg2};  
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = arg1;
+const limit = arg2
+const values = [`%${cohortName}%`, limit];
+
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    })
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error(err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,18 +10,25 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-pool.query(`
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 INNER JOIN teachers ON teachers.id = teacher_id
 INNER JOIN students ON students.id = student_id
 INNER JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${arg1}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`)
+`;
+
+const cohortName = arg1;
+const values = [`%${cohortName}%`];
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`)
+    })
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
